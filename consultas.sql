e1e4dd17fa3790872011f3e2caa681a94ed1b641
Luiz Fernando Rabelo <61765686+luizfernandorabelo@users.noreply.github.com>
2022-07-04 07:02:48 -0300

    Update consultas.sql

--- consultas.sql
+++ consultas.sql
@@ -27,3 +27,18 @@ WHERE (
     FROM aula a2 JOIN aula_assistida b ON a2.id = b.aula
     WHERE a.id = a2.id AND b.aluno = p.usuario
 ) = 1;
+
+-- Lista nome de todos os alunos, e, se houver, conteúdo e matéria da aula de mensagens enviadas por ele em fóruns a partir de 2022
+SELECT u.nome, m.conteudo, c.codigo
+FROM usuario u LEFT JOIN mensagem m ON u.username = m.usuario
+               LEFT JOIN discussao d ON m.discussao = d.id
+               LEFT JOIN aula a ON d.aula = a.id
+               LEFT JOIN disciplina c ON a.disciplina = c.codigo
+WHERE data_msg IS NULL OR EXTRACT (YEAR FROM data_msg) >= 2022;
+
+-- Lista da média de minutos das aulas relacionadas a disciplinas de cálculo por professor
+SELECT p.nome, AVG(duracao)
+FROM aula a JOIN disciplina d ON a.disciplina = d.codigo
+            JOIN usuario p ON a.professor = p.username
+WHERE d.codigo LIKE 'CALC_'
+GROUP BY p.username;
