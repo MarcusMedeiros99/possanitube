b731ecf02cc8f2dc45eb926e4441921b4e17ea0c
Luiz Fernando Rabelo <luizfernando2002@hotmail.com>
2022-07-04 01:58:20 -0300

    Update consultas.sql

--- consultas.sql
+++ consultas.sql
@@ -7,3 +7,25 @@ FROM aula a JOIN aula_assistida b
 ON a.id = b.aula
 GROUP BY a.id
 ORDER BY views DESC, likes DESC;
+
+
+-- Lista os professores ordenados por likes
+SELECT a.professor,
+COUNT(CASE WHEN b.avaliacao = 1 THEN 1 END) AS likes,
+COUNT(CASE WHEN b.avaliacao = -1 THEN 1 END) AS dislikes
+FROM aula a JOIN aula_assistida b
+ON a.id = b.aula
+GROUP BY a.professor
+ORDER BY likes DESC, dislikes ASC;
+
+
+-- Lista as aulas que receberam avaliação positiva E foram colocadas em alguma playlist por um mesmo usuário
+SELECT a.titulo, p.usuario, p.titulo, p.data_criacao
+FROM aula a JOIN aula_playlist ap ON a.id = ap.aula
+			JOIN playlist p ON ap.playlist = p.id
+WHERE (
+	SELECT b.avaliacao
+	FROM aula a2 JOIN aula_assistida b ON a2.id = b.aula
+	WHERE a.id = a2.id AND b.aluno = p.usuario
+) = 1;
+
